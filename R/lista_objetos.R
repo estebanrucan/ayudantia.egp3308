ca91db2366e69f6b7b898e5cabb461b747f82a52
estebanrucan <errucan@uc.cl>
2021-03-27 16:19:20 -0300

    cambios en lista_objetos

--- R/lista_objetos.R
+++ R/lista_objetos.R
@@ -7,7 +7,7 @@
 #'   \item{data_frame}{Tibble 'diamonds' del package \code{ggplot2}}
 #'   \item{matriz}{Una matriz de 100x100 con valores simulados desde una ditribución uniforme con parámetros -10 y 10}
 #'   \item{sub_lista}{Una lista que contiene 20 vectores numéricos con 20 elementos cada uno, obtenidos desde una distribución exponencial con parámetro 20}
-#'   \item{vector_palabras}{Un vector del tipo \code{character} con 100 elementos extraidos desde el package \code{string}}
+#'   \item{vector_palabras}{Un vector del tipo \code{character} con 100 elementos extraidos desde el package \code{stringr}}
 #' }
 #' @source \code{ggplot2}, \code{stringr}
 'lista_objetos'
